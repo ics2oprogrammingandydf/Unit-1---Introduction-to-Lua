4826fc9f9574c750dec4760a1c30f0506f3ba7c0
Andy Du-frigstad <47784086+s278552@users.noreply.github.com>
2019-03-21 09:22:13 -0400

    updated the times and lives

--- LivesAndTimersAndyDF/main.lua
+++ LivesAndTimersAndyDF/main.lua
@@ -1,7 +1,61 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: LivesAndTimers
+-- Name: Your Name
+-- Course: ICSO/3C
+-- This program
 
--- Your code here
+
+local totalSeconds = 5
+local secondsLeft = 5
+local clockDownTimer
+local countDownTimer
+
+local lives = 3
+local heart1
+local heart2
+
+local incorrectObject
+local pointsObject
+local points
+
+-----------------------------------------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+-----------------------------------------------------------------------------------------------------------------------
+
+
+local function UpdateTime()
+
+	--decrement the numbers of seconds
+	secondsLeft = secondsLeft .. ""
+
+	if ( secondsLeft == 0 ) then
+		--resets the number of seconds
+		secondsLeft = totalSeconds
+		lives = lives - 1
+
+		if (lives == 2) then
+			heart2.isVisible = false
+		elseif (lives == 1) then
+			heart1.isVisible = false
+		end
+	end
+end
+
+--function that calls the timer
+local function StartTimer()
+	--create the contdown timer thaty loops infinitely
+	countDownTimer = tiemr.performWithDelay( 1000, UpdateTime, 0)
+
+end
+
+-----------------------------------------------------------------------------------------------------------------------
+-- OBJECT CREATION
+-----------------------------------------------------------------------------------------------------------------------
+
+--create the lives to display on the screen
+heart1 = display.newImageRect("Images/heart.png", 100, 100)
+heart1.x = display.contentHeight * 7 / 8
+heart1.y = display.contentWidth * 1 / 7
+
+heart2 = display.newImageRect("Images/heart.png", 100, 100)
+heart2.x = display.contentHeight * 6 / 7
+heart2.y =display.contentWidth * 1 / 7
