9f6c3aa14b6395edeca26f8aa55805f8b65b3e40
Andy Du-frigstad <47784086+s278552@users.noreply.github.com>
2019-03-28 08:58:12 -0400

    fsdg

--- WhackAMoleAndyDF/main.lua
+++ WhackAMoleAndyDF/main.lua
@@ -1,7 +1,9 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: WhackAMole
+-- Name: Your Name
+-- Course: ICS2O/3C
+-- This program places a random object on the screen. if the user clicks on it in time
+-- the score increases by 1.
 
--- Your code here
+display.setStatusBar(display.HiddenStatusBar)
+
+display.newRect
