ef6db70297c355274b287131b38cc3bd75968620
Andy Du-frigstad <47784086+s278552@users.noreply.github.com>
2019-04-01 09:17:51 -0400

    did the whack a mole

--- WhackAMoleAndyDF/main.lua
+++ WhackAMoleAndyDF/main.lua
@@ -6,4 +6,21 @@
 
 display.setStatusBar(display.HiddenStatusBar)
 
-display.newRect
+local bkg = display.newRect( 0, 0, display.contentWidth, display.contentHeight)
+	display.setDefault("background", 153/255, 0/255, 0/255)
+
+	-- setting the position
+	bkg.anchorX = 0
+	bkg.anchorY = 0
+	bkg.x = 0
+	bkg.y = 0
+
+-- creating the mole
+local mole = display.newImage ( "Images/mole.png" , 0, 0 )
+	mole:scale (0.3333333333333333333333333333333333333333333333333333333333, 0.3333333333333333333333333333333333333333333333333333333333)
+	
+	--setting the position
+	mole.x = display.contentCenterX
+	mole.y = display.contentCenterY
+	mole.isVisible = false
+
