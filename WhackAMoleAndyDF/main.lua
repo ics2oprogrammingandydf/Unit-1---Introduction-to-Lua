2eea6cacf2a2224a191186481c5f1fe1a9bffacd
Andy Du-frigstad <47784086+s278552@users.noreply.github.com>
2019-04-01 09:37:28 -0400

    did more whacka  mole stud=ff

--- WhackAMoleAndyDF/main.lua
+++ WhackAMoleAndyDF/main.lua
@@ -24,3 +24,49 @@ local mole = display.newImage ( "Images/mole.png" , 0, 0 )
 	mole.y = display.contentCenterY
 	mole.isVisible = false
 
+------------------------------------------------------ Functions--------------------------------------------------------------------------------\
+
+-- this function that makes the mole appears in a random X y position on the screen before calling the Hide Function
+function PopUp( )
+
+	---CHoosing random postion on the screen between 0 and the size of the screen
+	mole.x = math.random( 0, display.contentWidth )
+	mole.y = math.random( 0, display.contentHeight )
+	mole.isVisible = true
+
+end
+
+--this function calls the popup function after 3 seconds
+function PopUpDelay( )
+	timer.performWithDelay( 3000, PopUp )
+end
+
+--this function makes the mole invisible and then calls the PopUpDelay function
+function Hide( )
+
+	--Changing Visibilty
+	mole.isVisible = false
+
+end
+
+--This function starts the game
+function GameStart( )
+	PopUpDelay
+end
+
+-- this function increments the score only if the mole is clicked. It then displays the new score
+function Whacked( event )
+
+	--If touch phase just started
+	if (event.phase == "began") then
+
+
+	end
+
+end
+
+----------------------------------------------------Event Listener------------------------------------------
+-- I add the event listener to the moles so that if the mole i stouched, the whacked function is called
+mole:addEventListener( "touch", Whacked )
+
+-------------------------------------------------Start the game-----------------------------------------------------
